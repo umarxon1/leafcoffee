ee460cea1ccb64bfc29f5d3a4734d7f22680a96b
Umarxon1 <umarxonqwerty@gmail.com>
2025-10-04 00:06:59 +0500

    testimonial carousel changed

--- src/components/Testimonials/testimonials.tsx
+++ src/components/Testimonials/testimonials.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import { useRef } from "react";
 import { Carousel } from "antd";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { testimonials } from "@/data/db";
 import TestimonialCard from "./components/testimonialsCard";
 
-type CarouselRefType = {
-  next: () => void;
-  prev: () => void;
-};
+
 
 export default function Testimonials() {
-
-  const carouselRef = useRef<CarouselRefType>(null);
+  const contentStyle: React.CSSProperties = {
+  margin: 0,
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 
   return (
     <section className="bg-[#f9f6f1] py-16">
@@ -34,29 +35,15 @@ export default function Testimonials() {
         </div>
 
         <div className="md:hidden relative">
-          <Carousel dots ref={carouselRef as any}>
-            {testimonials.map((t) => (
+            <Carousel arrows infinite={true} autoplay autoplaySpeed={5000}>
+       {testimonials.map((t) => (
               <div key={t.id} className="px-4">
                 <div className="max-w-md mx-auto">
                   <TestimonialCard t={t} />
                 </div>
               </div>
             ))}
-          </Carousel>
-
-          <button
-            onClick={() => carouselRef.current?.prev()}
-            className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white shadow-md p-2 rounded-full"
-          >
-            <LeftOutlined style={{ fontSize: "18px" }} />
-          </button>
-
-          <button
-            onClick={() => carouselRef.current?.next()}
-            className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white shadow-md p-2 rounded-full"
-          >
-            <RightOutlined style={{ fontSize: "18px" }} />
-          </button>
+    </Carousel>
         </div>
       </div>
     </section>
